a02bb18f17816f9d3acd2501b9da66a7dea553b5
James White <whitejaad@gmail.com>
2022-07-11 21:01:12 -0700

    Update Friendslist2

--- friendslist.rb
+++ friendslist.rb
@@ -1,3 +1,9 @@
 # These are my friends
 
-friends_list = "Al", "Joel", "Wes", "Shane", "Jon"
+puts friends_list = "Al", "Joel", "Wes", "Shane", "Jon"
+
+puts number_children_each_have = "Al3", "Joel1", "Wes1", "Shane2", "Jon3"
+
+puts friends_ages = "Al38", "Joel37", "Wes38", "Shane45", "Jon45"
+
+puts Friend1 = ["Friend1Allan", "Friend2Joel"]
